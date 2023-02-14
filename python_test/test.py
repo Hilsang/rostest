bc16fe1b9eae318c35934851b09b93262f945ddd
Hilsang <direrld@naver.com>
2023-02-13 18:14:50 -0800

    cc

---
+++ python_test/test.py
@@ -0,0 +1,24 @@
+class Test():
+
+def __init__(self):
+  self.var = 10
+  self.var2 = 20
+
+def aa(self):
+  print(aaaabb)
+
+class Inh(Test):
+
+  def __init(self):
+  super().__init__()
+
+def main():
+  a = 10
+  a = 'hello'
+  b = int()
+  print(a)
+  ob = Test()
+  ob.aa()
+  ob.aa()
+  print(ob.var)
+  print(ob.var2)
