0e80a976489cab82b8e5e9db8bb671cddb6f8644
Hilsang <direrld@naver.com>
2023-02-13 20:47:03 -0800

    j

--- my_package/mpub.py
+++ my_package/mpub.py
@@ -6,15 +6,20 @@ class M_pub(Node):
   def __init__(self):
     super().__init__('mpub')
     self.pub = self.create_publisher(String, 'messagepub', 10)
+    self.create_timer(1, self.pub.pubmessage)
+
+    def pubmessage(self):
+      msg = String()
+      msg.data = 'hello world'
+      self.pub.publish(msg)
 
 def main():
   rclpy.init()
   node = M_pub()
-  msg = String()
-  msg.data = 'hello world'
-  while True:
-    node.pub.publish(msg)
-  node.destroy_node()
+  try:
+    rclpy.spin()
+  except:
+    node.destroy_node()
 
 if __name__ == '__main__':
   main()
